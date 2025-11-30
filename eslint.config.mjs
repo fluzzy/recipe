e6d8a532bf568b145c5f60a63741b2eb1ac2f1ea
fluzzy <nick02190219@gmail.com>
2025-11-30 17:01:31 +0900

    chore: eslint import order 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
-import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -11,11 +12,25 @@ const eslintConfig = defineConfig([
   {
     plugins: {
       prettier,
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
     },
     rules: {
       'prettier/prettier': 'warn',
       '@typescript-eslint/ban-ts-comment': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
       'react/self-closing-comp': [
         'warn',
         {
@@ -23,11 +38,22 @@ const eslintConfig = defineConfig([
           html: true,
         },
       ],
-      '@typescript-eslint/no-unused-vars': [
+      'import/order': [
         'warn',
         {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
         },
       ],
     },
